6ca9ebcb998c231b4c47be051324bc1d779771a1
XEPHERXII <75206317+XEPHERXII@users.noreply.github.com>
2023-02-08 09:21:58 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,25 +22,3 @@ for fname in glob("bin/*"):
 with open("requirements.txt") as f:
     dependencies = [line for line in f]
 
-setup(
-    name="ReFinED",
-    version="1.0",
-    description="Entity Linking via fine-grained entity typing",
-    author="Amazon Alexa AI",
-    author_email="apierleo@amazon.com",
-    # declare your packages
-    packages=find_packages(where="src", exclude=("test",)),
-    package_dir={"": "src"},
-    install_requires=dependencies,
-    include_package_data=True,
-    # include data files
-    data_files=data_files,
-    test_command="none",
-    doc_command="none",
-    # Enable build-time format checking
-    check_format=False,
-    # Enable type checking
-    test_mypy=False,
-    # Enable linting at build time
-    test_flake8=False,
-)
